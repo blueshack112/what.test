81663547700c870b5b8728e84bdb767b685ff935
hassanahmed <ahmed.hassan.112.ha@gmail.com>
2023-05-15 17:38:21 +0500

    jwt token decoding logic removed

--- frontend/src/lib/DecodedJWTTokenContext/typedefs.js
+++
@@ -1,8 +0,0 @@
-//@flow
-export type JwtToken = {
-  token_type?: string,
-  exp?: number,
-  iat?: number,
-  jti?: number,
-  user_id: number,
-};
--- frontend/src/modules/app/components/PrivateRoutes/PrivateRoutes.js
+++ frontend/src/modules/app/components/PrivateRoutes/PrivateRoutes.js
@@ -2,21 +2,17 @@
 import React, { useMemo } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import * as loadables from 'lib/loadables';
-import { DecodedJwtTokenContext } from 'lib/DecodedJWTTokenContext/DecodedJWTTokenContext';
 import storageService from 'lib/storageService';
-import jwtDecode from 'jwt-decode';
 
 const PrivateRoutes = () => {
   const location = useLocation();
   const token = storageService.get('token');
-  const tokenDecoded = useMemo(() => (token ? jwtDecode(token) : null), [token]);
+
   return (
     <>
-      <DecodedJwtTokenContext.Provider value={tokenDecoded}>
-        <Routes location={location}>
-          <Route path={'/*'} element={<loadables.LoadableDashboardModuleTemplateWithRoutes />} />
-        </Routes>
-      </DecodedJwtTokenContext.Provider>
+      <Routes location={location}>
+        <Route path={'/*'} element={<loadables.LoadableDashboardModuleTemplateWithRoutes />} />
+      </Routes>
     </>
   );
 };
