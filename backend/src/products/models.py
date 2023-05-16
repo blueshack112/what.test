c5ba4ac285309bf78cd6cb86d67f077840ebf36e
Hassan Ahmed DC <ahmed.hassan.112.ha@gmail.com>
2023-05-16 18:00:11 +0500

    create active selection object

--- backend/src/products/models.py
+++ backend/src/products/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils.text import slugify
 
+from utils.enhancements import get_user_model
+
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
@@ -13,5 +15,14 @@ class Product(models.Model):
         self, force_insert=False, force_update=False, using=None, update_fields=None
     ):
         self.slug = slugify(self.name)
-
         super().save(force_insert, force_update, using, update_fields)
+
+
+class ActiveSearchAndSelection(models.Model):
+    user = models.OneToOneField(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="active_search_and_selection",
+    )
+    selected_products = models.ManyToManyField(Product)
+    search_query = models.TextField(null=True, blank=True)
