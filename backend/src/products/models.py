1996bc2bbff2b485de1316152cb9bad2488a09c7
hassanahmed <ahmed.hassan.112.ha@gmail.com>
2023-05-16 13:40:47 +0500

    add slug field

--- backend/src/products/models.py
+++ backend/src/products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Product(models.Model):
@@ -6,3 +7,11 @@ class Product(models.Model):
     description = models.TextField(null=True, blank=False)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     stock = models.IntegerField()
+    slug = models.SlugField(null=True, blank=False)
+
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        self.slug = slugify(self.name)
+
+        super().save(force_insert, force_update, using, update_fields)
