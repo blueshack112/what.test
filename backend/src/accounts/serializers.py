4c884beabb4d62b804ea73aef8376e5d28986cb4
Hassan Ahmed DC <ahmed.hassan.112.ha@gmail.com>
2023-05-16 19:09:23 +0500

    active search and selection serializer

--- backend/src/accounts/serializers.py
+++ backend/src/accounts/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
+from accounts.models import ActiveSearchAndSelection
 from utils.enhancements import get_user_model
 
 
+class ActiveSearchAndSelectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ActiveSearchAndSelection
+        fields = "__all__"
+
+
 class UserSerializer(serializers.ModelSerializer):
+    active_search_and_selection = ActiveSearchAndSelectionSerializer()
+
     class Meta:
         model = get_user_model()
         fields = [
@@ -15,5 +24,6 @@ class UserSerializer(serializers.ModelSerializer):
             "email",
             "date_joined",
             "is_superuser",
+            "active_search_and_selection",
         ]
         read_only_fields = ["id", "last_login", "date_joined", "is_superuser"]
