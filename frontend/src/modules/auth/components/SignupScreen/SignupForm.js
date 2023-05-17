dd71be7822b5f4498b448a18d7909d0d13c69c4a
Hassan Ahmed DC <ahmed.hassan.112.ha@gmail.com>
2023-05-17 13:09:54 +0500

    fix lint errors

--- frontend/src/modules/auth/components/SignupScreen/SignupForm.js
+++ frontend/src/modules/auth/components/SignupScreen/SignupForm.js
@@ -23,7 +23,6 @@ import { Link, useNavigate } from 'react-router-dom';
 import { btnStyle, inputSX } from '../../styles';
 import { useCrudAction } from 'lib/comms_v2/nonGetActions';
 import { compileSignupAction } from '../../actionCreators';
-import { logIn } from '../../workflows';
 import { colorPalette } from 'lib/constants';
 import { toast } from 'react-toastify';
 
