9a60d660d489edecf4c8ab08b8b16cc62176248a
Hassan Ahmed DC <ahmed.hassan.112.ha@gmail.com>
2023-05-17 10:44:03 +0500

    add whitenoise middleware

--- backend/src/what_test/settings_base.py
+++ backend/src/what_test/settings_base.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
